3857a4d074d12e021a6f31285f0a93d2872ec62c
Rishabh Tatiraju <me@rishabh.blog>
2021-03-09 00:13:11 +0530

    UI changes to under construction

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -4,11 +4,11 @@ import GuitarPlayerImage from '../assets/guitar-player.svg';
 
 const UnderConstruction = () => {
     return (
-        <Row className="p-0 m-0 align-self-center">
-            <Col md={4} className="p-0 m-0">
+        <Row className="p-0 m-0 pt-5 justify-content-center">
+            <Col md={4} className="p-0 m-0 align-self-center">
                 <img src={GuitarPlayerImage} width="100%"/>
             </Col>
-            <Col className="p-0 m-0 align-self-center" md={8}>
+            <Col className="p-0 m-0 align-self-center" md={4}>
                 <h1 className="bold-text">Mi... So... Fa...</h1>
                 <p className="regular-text">A good piece is being written. Check back later!</p>
             </Col>
